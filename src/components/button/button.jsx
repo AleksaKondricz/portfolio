3ef79e95cfc99de5631ab815bae3ec96a123be35
AleksaKondricz <al.nikolovska@hotmail.com>
2020-03-04 22:15:50 +0100

    Updates

--- src/components/button/button.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from './button.module.css'
-
-const Button = ({label, onClick, type}) => {
-return (<button  className={styles[type]} onClick={onClick}>{label}</button>  );
-}
- 
-export default Button;
